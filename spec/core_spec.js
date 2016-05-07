e92eaddc6fac87a6c16de31940eddef23169cc39
Daniel Pyrathon <daniel@coffeemeetsbagel.com>
2016-05-07 15:15:30 -0700

    better schema

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -1,15 +1,38 @@
 var Node = require('../src/core.js').Node;
 var NodeType = require('../src/core.js').NodeType;
+var Vertex = require('../src/core.js').Vertex;
 
 describe('Node', () => {
     it('should be able to be a sink', () => {
-        var node = new Node(NodeType.SINK);
+        var node = new Node('a', NodeType.SINK);
         expect(node.nodeType).toBe(NodeType.SINK);
+        expect(node.name).toBe('a');
     });
 
     it('should raise an exception if its invalid', () => {
         expect(() => {
             new Node("invalid");
         }).toThrow()
+    });
+});
+
+describe('Vertex', () => {
+    it('should be able to have a capacity and a flow', () => {
+        var n1 = new Node('a', NodeType.NODE);
+        var n2 = new Node('b', NodeType.NODE);
+        var vertex = new Vertex(n1, n2, 5, 0);
+        expect(vertex.getCapacity()).toBe(5);
+        expect(vertex.getFlow()).toBe(0);
+    });
+
+    it('should be able to register', () => {
+        var n1 = new Node('a', NodeType.NODE);
+        var n2 = new Node('b', NodeType.NODE);
+        var vertex = new Vertex(n1, n2, 5, 0);
+
+        expect(n1.getOutgoingEdges()).toEqual([vertex]);
+        expect(n1.getIncomingEdges()).toEqual([]);
+        expect(n2.getOutgoingEdges()).toEqual([]);
+        expect(n2.getIncomingEdges()).toEqual([vertex]);
     });
 });
--- src/core.js
+++ src/core.js
@@ -7,15 +7,52 @@ var NodeType = {
 }
 
 
+class Vertex {
+    constructor(fromNode, toNode, capacity, flow) {
+        this.from = fromNode;
+        this.to = toNode;
+        this.capacity = capacity;
+        this.flow = flow;
+
+        this.bind(this.from, this.to);
+    }
+
+    bind(from, to) {
+        from.outgoing.push(this);
+        to.incoming.push(this);
+    }
+
+    getCapacity() {
+        return this.capacity;
+    }
+
+    getFlow() {
+        return this.flow;
+    }
+}
+
 class Node {
-    constructor(nodeType) {
+    constructor(name, nodeType) {
         var possibleTypes = new Set(Object.keys(NodeType));
         if (!possibleTypes.has(nodeType)) {
             throw "type is invalid";
         }
         this.nodeType = nodeType;
+        this.name = name;
+        
+        this.outgoing = [];
+        this.incoming = [];
+    }
+
+    getOutgoingEdges() {
+        return this.outgoing;
+    }
+
+    getIncomingEdges() {
+        return this.incoming;
     }
 }
 
 exports.Node = Node;
 exports.NodeType = NodeType;
+exports.Vertex = Vertex;
